6b1a52f2bcf06d9aa400f10e0bfa7ad96e08e03b
yeferlol12 <ysantamaria@cesde.net>
2025-09-18 09:54:04 -0500

    portada

---
+++ inicio.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.markdown(
+    """
+    <div style="text-align:center;">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgi6IeLDBsBuwszwBU-qb_OfzPVEKFA4gDGg&s" width="400">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title("Nuevas tecnologias ")
+st.subheader("Presentado por:")
+
+st.text("Yorman Ivan Usme")
+st.text("Yeferson Santamaria Holguin")
+st.text("Juan")
+
+st.title("Bienvenido a nuestra aplicación")
+
+st.write("""
+  Esta es basada en la base de datos de las contratacion de medellin, proyectos , estados 
+         en los que esta el proyecto si terminado y pagos, en esta plataforma tratamos de darle graficos de
+         cada columna estados de obra y pagos. 
+""")
+
+
+
+
+    
+
