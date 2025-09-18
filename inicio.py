dd2f1ed78e24aa254340c0f5f854ecf82d429730
yeferlol12 <ysantamaria@cesde.net>
2025-09-18 10:13:22 -0500

    acualizacion de datos de la portada

--- inicio.py
+++ inicio.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.markdown(
     """
     <div style="text-align:center;">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgi6IeLDBsBuwszwBU-qb_OfzPVEKFA4gDGg&s" width="400">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgi6IeLDBsBuwszwBU-qb_OfzPVEKFA4gDGg&s" width="300">
     </div>
     """,
     unsafe_allow_html=True
@@ -19,9 +19,15 @@ st.text("Juan")
 st.title("Bienvenido a nuestra aplicación")
 
 st.write("""
-  Esta es basada en la base de datos de las contratacion de medellin, proyectos , estados 
-         en los que esta el proyecto si terminado y pagos, en esta plataforma tratamos de darle graficos de
-         cada columna estados de obra y pagos. 
+    En esta aplicacion prodas encotrar nuestro proyecto final.
+    tambien te vamos hablor de la analitica de una base de datos de contraciones en medellin.
+     
+    En cuanto a nuestro proyecto final, vamos a mostrar los graficos que genera la base de datos que vamos a 
+    utilizar desde sql, de como se consume esta mediante una api rest y de como se puede mostrar mediante pyton.     
+
+    Desde la analitica nos basamos en la base de datos de las contratacion de medellin, proyectos , estados 
+    en los que esta el proyecto si terminado y pagos, en esta plataforma tratamos de darle graficos de
+    cada columna estados de obra y pagos. 
 """)
 
 
